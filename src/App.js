d7d52ef559c579045275b7a2ba18a0621172afc0
Tischkante <89406622+Tischkante@users.noreply.github.com>
2021-08-23 19:23:32 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+import background from './goldi.jpg';
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundImage: `url(${background})`, objectFit: "contain" }}>
+    <h3>Impressum</h3>
+    <div id="lustigerText">
+        <p id="pTag">Wenn Sie jemanden verschwinden lassen wollen oder einfach mal Ruhe brauchen rufen sie mich Inkasseo Reins der Mann fürs Grobe.</p>
+    </div>
+        <div id="centerDiv">
+            <div id="flexWrap">
+                <p>Joel Reins</p>
+                <p>+49 179 2939098</p>
+                <a href="mailto:InkassoReins@yahoo.com">Mail me at InkassoReins@yahoo.com</a>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
